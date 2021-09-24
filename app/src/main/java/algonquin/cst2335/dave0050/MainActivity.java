a67d56fc9105ff9c5ebf02c6e6f27473a224c2eb
Vin <dave0050@algonquinlive.com>
2021-09-24 00:01:07 -0400

    sept 24 - midnight - added in edit text and buttons

--- app/src/main/java/algonquin/cst2335/dave0050/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/dave0050/MainActivity.java
@@ -3,6 +3,9 @@ package algonquin.cst2335.dave0050;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView mytext = findViewById(R.id.textview);
+
+        Button but = findViewById(R.id.mybutton);
+
+        EditText myedit = findViewById(R.id.myedittext);
+
+        but.onclick(View v);
+
     }
+
 }
