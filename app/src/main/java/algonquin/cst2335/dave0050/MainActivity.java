cee7379066cad90ee8eb147bfd98c46319ffc650
Vin <dave0050@algonquinlive.com>
2021-09-27 17:30:02 -0400

    Sept 27th - fixed all codes errors, project is working

--- app/src/main/java/algonquin/cst2335/dave0050/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/dave0050/MainActivity.java
@@ -2,9 +2,14 @@ package algonquin.cst2335.dave0050;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,34 +25,36 @@ public class MainActivity extends AppCompatActivity {
 
         EditText myedit = findViewById(R.id.myedittext);
 
-        String editString = myedit.gettext().toString();
+        String editString = myedit.toString();
 
 
         but.setOnClickListener(new View.OnClickListener() {
 
-            public void OnClick(View v) {
+            @Override
+            public void onClick(View view) {
                 mytext.setText("Your edit text has:" + editString);
 
             }
         });
 
-        Checkbox cb = findViewById(R.id.CB);
+        CheckBox cb = findViewById(R.id.CB);
 
-        Switch sw = findViewbyId(R.id.Switch);
+        Switch sw = findViewById(R.id.Switch);
 
-        RadioButton rd = findViewbyId(R.id.radio);
+        RadioButton rd = findViewById(R.id.radio);
 
-        cb.setOnCheckedListener(( b,c) -> {
+
+        cb.setOnCheckedChangeListener((b,c) -> {
             Toast.makeText(MainActivity.this, "You Clicked on Checkbox", Toast.LENGTH_LONG);
 
         });
 
-        sw.setOnCheckedListener(( b,c) -> {
+        sw.setOnCheckedChangeListener(( b,c) -> {
             Toast.makeText(MainActivity.this, "You Clicked on switch", Toast.LENGTH_SHORT);
 
         });
 
-        rd.setOnCheckedListener(( b,c) -> {
+        rd.setOnCheckedChangeListener(( b,c) -> {
             Toast.makeText(MainActivity.this, "You Clicked on radio", Toast.LENGTH_SHORT);
 
         });
