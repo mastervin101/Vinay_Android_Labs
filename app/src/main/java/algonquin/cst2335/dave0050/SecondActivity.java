ec244fb96b4fc4def0d5cfe1618d51eabdfd997e
Vin <dave0050@algonquinlive.com>
2021-10-05 12:41:49 -0400

    Added 2nd class, need to create new view

---
+++ app/src/main/java/algonquin/cst2335/dave0050/SecondActivity.java
@@ -0,0 +1,14 @@
+package algonquin.cst2335.dave0050;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState)A;
+        setContentView(R.layout.activity_second);
+    }
+}
