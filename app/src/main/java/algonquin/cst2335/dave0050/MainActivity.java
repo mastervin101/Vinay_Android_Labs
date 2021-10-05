6c089c5ff849e43efad4a10eba0dfe4dc36a141a
Vin <dave0050@algonquinlive.com>
2021-10-05 09:53:29 -0400

    Implemented the first functions

--- app/src/main/java/algonquin/cst2335/dave0050/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/dave0050/MainActivity.java
@@ -5,10 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
