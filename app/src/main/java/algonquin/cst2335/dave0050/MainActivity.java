9bb0f5665f71a7dbfad7d94d14ce0e426f5ab857
Vin <dave0050@algonquinlive.com>
2021-10-05 10:03:11 -0400

    Added logger text

--- app/src/main/java/algonquin/cst2335/dave0050/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/dave0050/MainActivity.java
@@ -3,6 +3,7 @@ package algonquin.cst2335.dave0050;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -14,25 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.w("Main Activity", "In onCreate() - Loading Widgets");
+
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        Log.w("Main Activity", "In onStop() - This application is no longer visible");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Log.w("Main Activity", "In onDestroy()- Any memory used by the application is freed");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        Log.w("Main Activity", "In onPause() - This application no longer responds to user input");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Log.w("Main Activity", "In onResume() - The application is now responding to user input");
     }
 }
