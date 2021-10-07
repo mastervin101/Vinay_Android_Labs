8c29b57d6499f43c06bafe4919241ce760b67655
Vin <dave0050@algonquinlive.com>
2021-10-06 23:06:00 -0400

    Added camera functionality and updated gradle dependencies, 11PM Oct 6th 2021

--- app/src/main/java/algonquin/cst2335/dave0050/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/dave0050/MainActivity.java
@@ -2,8 +2,11 @@ package algonquin.cst2335.dave0050;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -16,6 +19,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.w("Main Activity", "In onCreate() - Loading Widgets");
+
+        Button loginButton = findViewById(R.id.button);
+
+        EditText ET = findViewById(R.id.ET);
+
+        loginButton.setOnClickListener( clk -> {
+
+            Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
+
+            nextPage.putExtra("Email Address",ET.getText().toString());
+            startActivity(nextPage);
+
+        });
+
 
     }
 
--- app/src/main/java/algonquin/cst2335/dave0050/SecondActivity.java
+++ app/src/main/java/algonquin/cst2335/dave0050/SecondActivity.java
@@ -1,8 +1,21 @@
 package algonquin.cst2335.dave0050;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +23,51 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent fromPrevious = getIntent();
+        String emailAddress = fromPrevious.getStringExtra("Email Address");
+
+        TextView TV = findViewById(R.id.EM);
+
+        TV.setText("Welcome Home" + emailAddress);
+
+        Button Call = findViewById(R.id.Call);
+
+        EditText phone =findViewById(R.id.PN);
+
+        String phoneNumber = phone.getText().toString();
+
+        Call.setOnClickListener( clk -> {
+            Intent call = new Intent(Intent.ACTION_DIAL);
+            call.setData(Uri.parse("tel:"+ phoneNumber));
+            startActivity(call);
+
+
+        });
+
+        ImageView camera = findViewById(R.id.imageView);
+
+        camera.setOnClickListener( clk -> {
+
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(
+
+                    new ActivityResultContracts.StartActivityForResult(),
+                    new ActivityResultCallback<ActivityResult>() {
+                        @Override
+                        public void onActivityResult(ActivityResult result) {
+                            if (result.getResultCode() == Activity.RESULT_OK) {
+                                Intent data = result.getData();
+                                Bitmap thumbnail = data.getParcelableExtra("data");
+                                camera.setImageBitmap(thumbnail);
+                        }
+                    }
+                    }
+
+            );
+            cameraResult.launch(cameraIntent);
+        });
+
+
     }
 }
