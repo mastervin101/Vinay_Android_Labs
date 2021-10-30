e5fdf8923b51f5c806851d9518bd771491ab941f
vinnyd <dave0050@algonquinlive.com>
2021-10-30 15:40:39 -0400

    Week 6- Part 1 - 4PM Oct 30th 2021

--- app/src/main/java/algonquin/cst2335/dave0050/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/dave0050/MainActivity.java
@@ -3,6 +3,9 @@ package algonquin.cst2335.dave0050;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView TV = findViewById(R.id.text);
+        EditText ET = findViewById(R.id.Password);
+        Button btn = findViewById(R.id.Login);
     }
 }
